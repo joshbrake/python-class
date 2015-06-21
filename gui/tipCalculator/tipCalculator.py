83cd56c4e6319b2ad9570fabf532756bb4ba05b8
Josh Brake <brake.josh@gmail.com>
2015-06-21 07:45:36 -0700

    Updated tip calculator

--- gui/tipCalculator/tipCalculator.py
+++ gui/tipCalculator/tipCalculator.py
@@ -12,7 +12,7 @@ class tipCalculator(Tkinter.Tk):
     self.grid()
 
     # Meal subtotal label
-    self.subtotalLabel = Tkinter.Label(self,text='Subtotal ($)',anchor='w',fg='black')
+    self.subtotalLabel = Tkinter.Label(self,text='Subtotal ($)',anchor='c',fg='black')
     self.subtotalLabel.grid(column=0,row=0,sticky='EW')
 
     # Meal subtotal entry box
@@ -23,7 +23,7 @@ class tipCalculator(Tkinter.Tk):
     #self.subtotalEntry.selection_range(0,Tkinter.END)
 
     # Tip percentage label
-    self.tipPercentageLabel = Tkinter.Label(self,text='Tip Percentage (%)',anchor='w',fg='black')
+    self.tipPercentageLabel = Tkinter.Label(self,text='Tip Percentage',anchor='c',fg='black')
     self.tipPercentageLabel.grid(column=0,row=1,sticky='EW')
 
     # Tip percentage entry box
@@ -41,19 +41,22 @@ class tipCalculator(Tkinter.Tk):
 
     # Calculated tip label
     self.tipLabelVariable = Tkinter.StringVar()
-    self.tipLabel = Tkinter.Label(self,textvariable=self.tipLabelVariable,anchor='w',fg='black')
+    self.tipLabel = Tkinter.Label(self,textvariable=self.tipLabelVariable,anchor='c',fg='black')
     self.tipLabel.grid(column=0,row=2,columnspan=2,sticky='EW')
     self.tipLabelVariable.set('Tip: ?')
 
     # Calculated total
     self.totalLabelVariable = Tkinter.StringVar()
-    self.totalLabel = Tkinter.Label(self,textvariable=self.totalLabelVariable,anchor='w',fg='black')
+    self.totalLabel = Tkinter.Label(self,textvariable=self.totalLabelVariable,anchor='c',fg='black')
     self.totalLabel.grid(column=0,row=3,columnspan=2,sticky='EW')
     self.totalLabelVariable.set('Total: ?')
 
     # Calculate tip button
     self.button = Tkinter.Button(self,text="Calculate",command=self.calculate)
     self.button.grid(column=0,row=4,columnspan=2)
+    
+    # Set resizing
+    self.grid_columnconfigure((0,1),weight=1)
 
   def calculate(self):
     self.tipPercentage = self.tipDropdownVariable.get()
