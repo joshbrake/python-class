755e774031cae6f30aace8ce82ec3794dcc6080c
Josh Brake <brake.josh@gmail.com>
2015-08-06 05:20:13 +0000

    Updated comments

--- games/hangman.py
+++ games/hangman.py
@@ -6,25 +6,26 @@ from random import randint
 words = ["bear","righteous","holy","whiteboard","house","endocratic","judicial","window"]
 #words = ["test"]
 
+# Function to convert list to a string to print
 def list2string(l):
   string = "".join(l)
   return string
   
+# Function to setup the game by selecting a random word from the list and configure the display
 def setup(words):
-  #print len(words)
+
   # Generate random index to select random word from list "words"
   rand_index = randint(0,len(words)-1)
   
   # Choose the random word
   rand_word = words[rand_index]
   
-  #print rand_words
-  
   # Find the length of the random word to figure out how many blanks we need
   length_rand_word = len(rand_word)
 
   # Generate the display word full of blanks
   display_word_list = []
+  
   # Append an underscore to words
   for x in range(0,length_rand_word):
     display_word_list.append("_")
@@ -32,7 +33,7 @@ def setup(words):
   display_word = list2string(display_word_list)
   return rand_word,display_word_list
 
-
+# Function to draw hangman shape and fill in the appropriate number of body parts for number of wrong letters guessed
 def drawHangman(numWrong):
   
   numDisp = numWrong
@@ -52,18 +53,18 @@ def drawHangman(numWrong):
     "=============   \n"]
   print(''.join(hangman_template))
                    
-                
-
-#  print display_word[0]
-
+# Run setup
 (random_word,display_word_list) = setup(words)  
 
+# Setup for loop to run the main game
 keep_going = 1
 num_wrong_letters = 0
 max_num_wrong_letters = 7
 
+# Print the display word to the screen
 print(list2string(display_word_list))
 
+# Enter the main program loop
 while keep_going == 1:
   num_fails = 0
   user_guess = raw_input("Please pick a letter: ")
